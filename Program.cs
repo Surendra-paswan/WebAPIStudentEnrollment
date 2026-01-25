7e52db24e48ce6f84e5c81f4d9fe251fac73ab0b
Surendra Paswan <surendramoglaha@gmail.com>
2026-01-25 14:53:12 +0545

    add CancellationToken

--- Program.cs
+++ Program.cs
@@ -13,7 +13,9 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"),
+    
+    sql => sql.CommandTimeout(120)));
 
 // ? Register UnitOfWork
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
--- obj/Debug/net8.0/StudentRegistrationForm.AssemblyInfo.cs
+++ obj/Debug/net8.0/StudentRegistrationForm.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("StudentRegistrationForm")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b9d0b1103be9674d9ddc492768e05efa9dfcaf1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5510051e3f1d51d14435077e512b5be49340ebdc")]
 [assembly: System.Reflection.AssemblyProductAttribute("StudentRegistrationForm")]
 [assembly: System.Reflection.AssemblyTitleAttribute("StudentRegistrationForm")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
